30e2d2f58d41b5dcbf4f2b0ca7cffb53f8575f51
acalcutt <acalcutt@techidiots.net>
2015-03-09 18:54:16 -0400

    old fixes to the daily kml that got lost

--- wifidb/wifidb/lib/export.inc.php
+++ wifidb/wifidb/lib/export.inc.php
@@ -169,7 +169,7 @@ class export extends dbcore
         }
         return $daily_folder;
     }
-	
+
     public function ExportDailykmlOldVersion($date = NULL)
     {
         if($date === NULL)
@@ -179,14 +179,11 @@ class export extends dbcore
         $date_search = $date."%";
         $select_daily = "SELECT `id` , `points`, `username`, `title`, `date` FROM `wifi`.`user_imports` WHERE `date` LIKE '$date_search'";
         $result = $this->sql->conn->query($select_daily);
+
         if($this->sql->checkError(__LINE__, __FILE__))
         {
             $this->verbosed("There was an error running the SQL".var_export($this->sql->conn->errorInfo(), 1));
             Throw new ErrorException("There was an error running the SQL".var_export($this->sql->conn->errorInfo(), 1));
-        }
-        if($result->rowCount() < 1)
-        {
-            return -1;
         }
         
         if($this->named)
@@ -198,27 +195,7 @@ class export extends dbcore
             $this->verbosed("Start of Exporting Non-Labeled Daily KML.");
             $labeled = "";
         }
-        $fetch_imports = $result->fetchAll();
-        $data = array();
-        foreach($fetch_imports as $import)
-        {
-            $stage_pts = explode("-", $import['points']);
-            foreach($stage_pts as $point)
-            {
-                $exp = explode(":", $point);
-                $hash = $this->GetAPhash($exp[0]);
-                $id = $exp[0]+0;
-                $ret = $this->ExportSingleAP($id);
-                if(is_array($ret) && count($ret[$hash]['gdata']) > 0)
-                {
-                    $data = array_merge($data, $ret );
-                }
-            }
-            $this->createKML->ClearData();
-            $this->createKML->LoadData($data);
-            $KML_data = $this->createKML->PlotAllAPs(1, 1, $this->named);
-            $KML_data = $this->createKML->createFolder($import['username']." - ".$import['title'], $KML_data, 0);
-        }
+        
         $daily_folder = $this->daemon_out.$date;
         if(!@file_exists($daily_folder))
         {
@@ -229,17 +206,41 @@ class export extends dbcore
                 Throw new ErrorException("Error Making new Daily Export Folder. - ".$php_errormsg." - ".$daily_folder);
             }
         }
+
+        $fetch_imports = $result->fetchAll();
+        $KML_data="";
+        foreach($fetch_imports as $import)
+        {
+            $Import_KML_Data = "";
+            $stage_pts = explode("-", $import['points']);
+            foreach($stage_pts as $point)
+            {
+                $exp = explode(":", $point);
+                $hash = $this->GetAPhash($exp[0]);
+                $id = $exp[0]+0;
+                $ret = $this->ExportSingleAP($id);
+                if(is_array($ret) && count($ret[$hash]['gdata']) > 0)
+                {
+                    $this->createKML->ClearData();
+                    $this->createKML->LoadData($ret);
+                    $Import_KML_Data .= $this->createKML->PlotAllAPs(1, 1, $this->named);
+                }
+            }
+            if($Import_KML_Data != ""){$KML_data .= $this->createKML->createFolder($import['username']." - ".$import['title'], $Import_KML_Data, 0);}
+        }
+        if($KML_data == ""){$KML_data .= $this->createKML->createFolder("No Daily Exports with GPS", $KML_data, 0);}
         $full_kml_file = $daily_folder."/daily_db".$labeled.".kml";
         $this->verbosed("Writing the Daily KML File: ".$full_kml_file);
         $this->createKML->createKML($full_kml_file, "WiFiDB Daily Export ($date)", $KML_data);
         ##
         $link = $this->daemon_out.'daily_db'.$labeled.'.kml';
         $this->verbosed('Creating symlink from "'.$full_kml_file.'" to "'.$link.'"');
-        unlink($link);
+        unlink($link); 
         symlink($full_kml_file, $link);
         chmod($link, 0664);        
         #####################
         $this->verbosed("Starting to compress the KML to a KMZ.");
+
         $ret_kmz_name = $this->createKML->CreateKMZ($full_kml_file);
         if($ret_kmz_name == -1)
         {
@@ -260,8 +261,9 @@ class export extends dbcore
             symlink($ret_kmz_name, $link);
             chmod($link, 0664);
         }        
+
         return $daily_folder;
-    }	
+    }
 
     /*
      * Export All Daily Aps to KML
