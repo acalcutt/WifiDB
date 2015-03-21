2da2cda06ec321cd7b62b0022a5c45b6898cf2f8
Phillip Ferland <pferland@randomintervals.com>
2015-03-21 17:28:05 -0400

    .....

--- wifidb/tools/utilites/manufmac.php
+++ wifidb/tools/utilites/manufmac.php
@@ -1,89 +1,84 @@
 <?php
-$ver = "1.2.2";
-ini_set("memory_limit","3072M"); //lots of GPS cords need lots of memory
-$script_start = "2013-Jan-24";
-$last_edit = "2013-July-22";
-$author = "pferland";
+$ver = "1.2.1";
+ini_set("memory_limit","3072M");
+$script_start = "2009-Jan-24";
+$last_edit = "2013-07-04"; //Happy 4th of July!
+$author = "pferland"; //modified by acalcutt 2013-06-30 to support new oui.txt file
 $stime = time();
+//compile to exe using bamcomple 1.21 with command "bamcompile.exe manufmac.php manufmac.exe"
 echo "-----------------------------------------------------------------------\n";
-echo "| Starting creation of Vistumbler compatible Wireless Router Manuf List.\n| By: $author\n| http:\\\\www.wifidb.netn| Version: $ver\n";
+echo "| Starting creation of Vistumbler compatible Wireless Router Manuf List.\n| By: $author\n| http:\\www.randomintervals.com\n| Version: $ver\n";
 $debug = 0;
 $cwd = getcwd();
 
 $source="http://standards.ieee.org/develop/regauth/oui/oui.txt";
 $manuf_list = array();
 $phpfile = "manufactures.inc.php";
+$phpfilewrite = fopen($phpfile, "w");
+$phpfileappend = fopen($phpfile, "a");
+
 $vs1file = "manufactures.ini";
+$vs1filewrite = fopen($vs1file, "w");
+$vs1fileappend = fopen($vs1file, "a");
 
 echo "Downloading and Opening the Source File from: \n----->".$source."\n|\n|";
 $return = file($source);
 
 $total_lines = count($return);
 echo "Source File opened and Destination file placed, starting convertion.\n|\n|";
-foreach($return as $key=>$ret)
+
+foreach($return as $ret)
 {
-    #if($key == 40){die();}
-    if($ret == "" || $ret == "  \r\n")
-    {continue;}
-
-    #var_dump($ret);
-
-    preg_match("/([A-F0-9].+)     (\(base 16\))\t\t([a-zA-Z].+)/", $ret, $matches);
-
-    if(@$matches[2] != "(base 16)")
-    {
-        if($debug === 1)
-        {
-            echo "Erroneous data found, dropping\n| This is normal...\n| ";
-        }
-        continue;
-    }
-
-    #var_dump($matches);
-
-    if($matches[3] == "PRIVATE"){echo "Non Needed Manuf found...\n| ";continue;}
-
-    $manuf_list[] = array(
-                        "mac"   => str_pad($matches[1], 6, "0"),
-                        "manuf" => $matches[3]
-                    );
+	$test = substr($ret, 13,5);
+	if ($test != "(hex)"){if($debug === 1){echo "Erroneous data found, dropping\n| This is normal...\n| ";} continue;}
+	$retexp = explode("(hex)",$ret);
+	
+	$Man_mac = trim($retexp[0]);
+	$man_mac = explode("-",$Man_mac);
+	$Man_mac = implode("",$man_mac);
+	$Manuf = trim($retexp[1]);
+	if($Manuf == "PRIVATE"){echo "Non Needed Manuf found...\n| ";continue;}
+	$manuf_list[] = array(
+						"mac" 	=> $Man_mac,
+						"manuf"	=> addslashes($Manuf)
+						);
+			
 }
+echo "Manufactures and MAC Address' found...\n| ";
 $total_manuf = count($manuf_list);
-if($total_manuf > 0)
-{
-    echo "Manufactures and MAC Address' Found: $total_manuf\r\n";
-}else{
-    die("No Manufactures found... :(");
-}
+		echo "Write Manufactures File for both Vistumbler and WiFiDB:\n";
 
-echo "Write Manufactures File for both Vistumbler and WiFiDB:\r\n";
-$php_data = "<?php\r\n$"."manufactures=array(\r\n";
-$ini_data = ";This file allows you to assign a manufacturer to a mac address(first 6 digits).\r\n[MANUFACTURERS]\r\n";
+fwrite($vs1fileappend, ";This file allows you to assign a manufacturer to a mac address(first 6 digits).\r\n[MANUFACURERS]\r\n");
+fwrite($phpfileappend, "<?php\r\n$"."manufactures=array(\r\n");
+
+$current = 1;
 foreach($manuf_list as $manuf)
 {
-    #WiFiDB PHP Array
-    $php_data .= '"'.$manuf['mac'].'"=>"'.addslashes($manuf['manuf']).'",'."\r\n";
-    if($debug === 1){	echo $write."\n| ";}
+	if($total_manuf == $current)
+	{
+		$write = "\"".$manuf['mac']."\"=>\"".$manuf['manuf']."\"\r\n";
+	}else{
+		$write = "\"".$manuf['mac']."\"=>\"".$manuf['manuf']."\",\r\n";
+	}
+	if($debug === 1){	echo $write."\n| ";}
+	fwrite($phpfileappend, $write);
+	$current++;
+	
 
-    $current++;
-
-    #Vistumbler INI
-    if($debug == 1){echo $write."\r\n";}
-    $ini_data .= $manuf['mac']."=".$manuf['manuf']."\r\n";
+	fwrite($vs1fileappend, $manuf['mac']."=".$manuf['manuf']."\r\n");
+	if($debug == 1){echo $write."\n";}
 }
-$php_data .= ");\r?>";
-
-file_put_contents($vs1file, $ini_data);
-file_put_contents($phpfile, $php_data);
+fwrite($phpfileappend, ");\r?>");
 #------------------------------------------------------------------------------------------------------#
 
 $etime = time();
 $diff_time = $etime - $stime;
 $lines_p_min = $total_lines/$diff_time;
-    echo "Total Manufactures found: ".$total_manuf."\n----------------\n"
-    ."Start Time:.......".$stime."\n"
-    ."End Time:.........".$etime."\n"
-    ."Total Run Time:...".$diff_time."\n----------------\n"
-    ."Total Lines:......".$total_lines."\n"
-    ."Lines per min:....".$lines_p_min."\n"
+	echo "Total Manufactures found: ".$total_manuf."\n----------------\n"
+	."Start Time:.......".$stime."\n"
+	."End Time:.........".$etime."\n"
+	."Total Run Time:...".$diff_time."\n----------------\n"
+	."Total Lines:......".$total_lines."\n"
+	."Lines per min:....".$lines_p_min."\n"	 
 	."----------------\nDone";
+?>
