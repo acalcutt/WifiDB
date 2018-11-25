f5dec4b6e392ee05690c27889b335ebab21f240c
acalcutt <acalcutt@techidiots.net>
2018-11-25 18:32:26 -0500

    fix load geonames script

--- wifidb/tools/utilites/load_geonames.php
+++ wifidb/tools/utilites/load_geonames.php
@@ -18,7 +18,7 @@ if(@!$argv[1])
     exit("Need to used one of these switches (and only one, and it hast to be the first one):\r\n
         --admin1
         --admin2
-        --geoname
+        --geonames
         --countrynames\r\n");
 }
 
@@ -33,19 +33,14 @@ $l = 0;
 $II = 1;
 $sql = "";
 echo "Loading WiFiDB config files.\r\n";
-if(!(require(dirname(__FILE__).'/../daemon.config.inc.php'))){die("You need to create and configure your config.inc.php file in the [tools dir]/daemon/config.inc.php");}
-if($daemon_config['wifidb_install'] == ""){die("You need to edit your daemon config file first in: [tools dir]/daemon/config.inc.php");}
+if(!(require(dirname(__FILE__).'/../daemon.config.inc.php'))){die("You need to create and configure your [tools]/daemon.config.inc.php");}
+if($daemon_config['wifidb_install'] === ""){die("You need to edit your daemon config file first in: [tools dir]/daemon.config.inc.php");}
 require $daemon_config['wifidb_install']."/lib/init.inc.php";
 
-echo "Config files loaded. Going to try and connect to the SQL Server...\r\n";
-$conn = mysqli_connect($config['host'], $config['db_user'], $config['db_pwd'], "wifi");
-if(mysqli_connect_errno())
-{
-    die(mysqli_connect_errno());
-}
-echo "Connected, now lets LOAD ALL THE DATA!!!!\r\n";
 foreach ($contents as $line)
 {
+	$sql = "";
+	$line = trim(str_replace (array("\r\n", "\n", "\r"), ' ', $line));
     echo $line."\r\n";
     $tab_array = explode("\t", $line);
     $row = filter_var_array($tab_array, FILTER_SANITIZE_STRING);
@@ -57,41 +52,28 @@ foreach ($contents as $line)
     switch($load)
     {
         case "admin1":
-            $sql = "INSERT INTO `geonames_admin1` (`id`, `admin1`, `name`, `asciiname`, `geonameid`)
-                VALUES ('', '{$row[0]}','{$row[1]}','{$row[2]}','{$row[3]}')";
+            $sql = "INSERT INTO `geonames_admin1` (`admin1`, `name`, `asciiname`, `geonameid`)
+                VALUES ('{$row[0]}','{$row[1]}','{$row[2]}','{$row[3]}')";
             break;
-        case "geoname":
-            $sql = "INSERT INTO `geonames` (`id`, `geonameid`, `name`, `asciiname`, `alternatenames`, `latitude`, `longitude`,
+        case "geonames":
+            $sql = "INSERT INTO `geonames` (`geonameid`, `name`, `asciiname`, `alternatenames`, `latitude`, `longitude`,
     `feature_class`, `feature_code`, `country_code`, `cc2`, `admin1_code`, `admin2_code`, `admin3_code`, `admin4_code`,
     `population`, `elevation`, `gtopo30`, `timezone`, `mod_date`)
-    VALUES ('', '{$row[0]}','{$row[1]}','{$row[2]}','{$row[3]}','{$row[4]}','{$row[5]}','{$row[6]}','{$row[7]}','{$row[8]}','{$row[9]}','{$row[10]}','{$row[11]}','{$row[12]}','{$row[13]}','{$row[14]}','{$row[15]}','{$row[16]}','{$row[17]}','{$row[18]}');";
+    VALUES ('{$row[0]}','{$row[1]}','{$row[2]}','{$row[3]}','{$row[4]}','{$row[5]}','{$row[6]}','{$row[7]}','{$row[8]}','{$row[9]}','{$row[10]}','{$row[11]}','{$row[12]}','{$row[13]}','{$row[14]}','{$row[15]}','{$row[16]}','{$row[17]}','{$row[18]}');";
             break;
         case "admin2":
-            $sql = "INSERT INTO `geonames_admin2` (`id`, `admin2`, `name`, `asciiname`, `geonameid`)
-                VALUES ('', '{$row[0]}','{$row[1]}','{$row[2]}','{$row[3]}')";
+            $sql = "INSERT INTO `geonames_admin2` (`admin2`, `name`, `asciiname`, `geonameid`)
+                VALUES ('{$row[0]}','{$row[1]}','{$row[2]}','{$row[3]}')";
             break;
         case "countrynames":
-            $sql = "INSERT INTO `geonames_country_names` (`id`, `ISO`, `ISO3`, `ISO-Numeric`, `fips`, `Country`, `Capital`, `Area`, `Population`, `Continent`, `tld`, `CurrencyCode`, `CurrencyName`, `Phone`, `Postal Code Format`, `Postal Code Regex`, `Languages`, `geonamesid`, `neighbors`, `EquivalentFipsCode`)
-                VALUES ('', '{$row[0]}','{$row[1]}','{$row[2]}','{$row[3]}','{$row[4]}','{$row[5]}','{$row[6]}','{$row[7]}','{$row[8]}','{$row[9]}','{$row[10]}','{$row[11]}','{$row[12]}','{$row[13]}','{$row[14]}','{$row[15]}','{$row[16]}','{$row[17]}','{$row[18]}');";
+            $sql = "INSERT INTO `geonames_country_names` (`ISO`, `ISO3`, `ISO-Numeric`, `fips`, `Country`, `Capital`, `Area`, `Population`, `Continent`, `tld`, `CurrencyCode`, `CurrencyName`, `Phone`, `Postal Code Format`, `Postal Code Regex`, `Languages`, `geonamesid`, `neighbors`, `EquivalentFipsCode`)
+                VALUES ('{$row[0]}','{$row[1]}','{$row[2]}','{$row[3]}','{$row[4]}','{$row[5]}','{$row[6]}','{$row[7]}','{$row[8]}','{$row[9]}','{$row[10]}','{$row[11]}','{$row[12]}','{$row[13]}','{$row[14]}','{$row[15]}','{$row[16]}','{$row[17]}','{$row[18]}');";
             break;
     }
     echo $sql."\r\n";
-    #var_dump($row);
-    #echo count($row)."\r\n";
-    #die();
-
-    if(!mysqli_query($conn, $sql))
-    {
-        echo mysqli_error($conn)."\r\n";
-        die();
-        #mysqli_close($conn);
-        echo ".";
-    }
-    else
-    {
-        echo "$II Inserted!\r\n";
-    }
-    $sql = "";
+	$prep = $dbcore->sql->conn->prepare($sql);
+	$prep->execute();
+    
     $l = 0;
     $II++;
     if($r===0){echo "|\r";}
@@ -104,12 +86,6 @@ foreach ($contents as $line)
     if($r===70){echo "\\\r";$r=0;}
     $r++;
 }
-mysqli_close($conn);
-
-
-
-
-
 
 function parseArgs($argv){
     array_shift($argv);
@@ -141,40 +117,3 @@ function parseArgs($argv){
     }
     return $out;
 }
-?>
-
-
-
-
-
-
-<!--
-CREATE TABLE `geonames_country_names` 
-(
-    `id` int(255) NOT NULL AUTO_INCREMENT, 
-    `ISO` varchar(255) NOT NULL, 
-    `ISO3` varchar(255) NOT NULL, 
-    `ISO-Numeric` varchar(255) NOT NULL, 
-    `fips` varchar(255) NOT NULL, 
-    `Country` varchar(255) NOT NULL, 
-    `Capital` varchar(255) NOT NULL, 
-    `Area` varchar(255) NOT NULL, 
-    `Population` varchar(255) NOT NULL, 
-    `Continent` varchar(255) NOT NULL, 
-    `tld` varchar(255) NOT NULL, 
-    `CurrencyCode` varchar(255) NOT NULL, 
-    `CurrencyName` varchar(255) NOT NULL, 
-    `Phone` varchar(255) NOT NULL, 
-    `Postal Code Format` varchar(255) NOT NULL, 
-    `Postal Code Regex` varchar(255) NOT NULL, 
-    `Languages` varchar(255) NOT NULL, 
-    `geonamesid` varchar(255) NOT NULL, 
-    `neighbors` varchar(255) NOT NULL, 
-    `EquivalentFipsCode` varchar(255) NOT NULL,
-    
-     PRIMARY KEY (`id`), 
-     INDEX (`id`)
-)
-ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_bin;
-
--->
