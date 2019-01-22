78aba406679da2715fa6cda71b759c157e33e972
acalcutt <acalcutt@techidiots.net>
2019-01-22 00:01:21 -0500

    small sql change

--- wifidb/wifidb/index.php
+++ wifidb/wifidb/index.php
@@ -108,14 +108,14 @@ else
 }
 
 if($dbcore->sql->service == "mysql")
-	{$sql = "SELECT COUNT(`id`) AS `ApCount` FROM `files` WHERE `user` = ? And `ValidGPS` = 1";}
+	{$sql = "SELECT `ValidGPS` FROM `files` WHERE `user` = ? And `ValidGPS` = 1 LIMIT 1";}
 else if($dbcore->sql->service == "sqlsrv")
-	{$sql = "SELECT COUNT([id]) AS [ApCount] FROM [files] WHERE [user] = ? And [ValidGPS] = 1";}
+	{$sql = "SELECT TOP 1 [ValidGPS] FROM [files] WHERE [user] = ? And [ValidGPS] = 1";}
 $prep = $dbcore->sql->conn->prepare($sql);
 $prep->bindParam(1, $lastusername, PDO::PARAM_STR);
 $prep->execute();
 $appointer = $prep->fetch(2);
-if($appointer['ApCount'] !== "0")
+if($appointer['ValidGPS'] == 1)
 {
 	$user_globe_html = "<a href=\"".$dbcore->URL_PATH."opt/map.php?func=user_all&labeled=0&user=".$lastusername."\" title=\"Show User APs on Map\"><img width=\"20px\" src=\"".$dbcore->URL_PATH."img/globe_on.png\"></a>";				
 	$user_globe_html .= "<a href=\"".$dbcore->URL_PATH."api/geojson.php?json=1&func=exp_user_all&user=".$lastusername."\" title=\"Export User APs to JSON\"><img width=\"20px\" src=\"".$dbcore->URL_PATH."img/json_on.png\"></a>";							
