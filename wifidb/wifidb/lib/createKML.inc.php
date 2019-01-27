c37620199d096ce8e4e20695a1d04cecaa8dfe42
acalcutt <acalcutt@techidiots.net>
2019-01-27 01:52:20 -0500

    fixed bad chars in kmz export

--- wifidb/wifidb/lib/createKML.inc.php
+++ wifidb/wifidb/lib/createKML.inc.php
@@ -304,7 +304,7 @@ class createKML
 				$sec_type_label = "open";
 				break;
 		}
-		$ap_ssid = htmlspecialchars(dbcore::formatSSID($ap_info_array['ssid']));
+		$ap_ssid = dbcore::formatSSID($this->stripInvalidXml(htmlspecialchars($ap_info_array['ssid'])));
 		if($ap_info_array['named'])
 		{	
 			$named = "			<name>".$ap_ssid."</name>";
@@ -775,5 +775,42 @@ class createKML
 		} else {
 			return -2;
 		}
+	}
+	
+	/**
+	 * Removes invalid XML
+	 *
+	 * @access public
+	 * @param string $value
+	 * @return string
+	 */
+	function stripInvalidXml($value)
+	{
+		$ret = "";
+		$current;
+		if (empty($value)) 
+		{
+			return $ret;
+		}
+
+		$length = strlen($value);
+		for ($i=0; $i < $length; $i++)
+		{
+			$current = ord($value{$i});
+			if (($current == 0x9) ||
+				($current == 0xA) ||
+				($current == 0xD) ||
+				(($current >= 0x20) && ($current <= 0xD7FF)) ||
+				(($current >= 0xE000) && ($current <= 0xFFFD)) ||
+				(($current >= 0x10000) && ($current <= 0x10FFFF)))
+			{
+				$ret .= chr($current);
+			}
+			else
+			{
+				$ret .= " ";
+			}
+		}
+		return $ret;
 	}
 }
