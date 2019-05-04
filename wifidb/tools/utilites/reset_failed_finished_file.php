d241ed5ffb8abdf8ed75e886a2befb7c6d09c1ac
acalcutt <acalcutt@techidiots.net>
2019-05-04 17:37:55 -0400

    updated reset scripts

--- wifidb/tools/utilites/reset_failed_finished_file.php
+++ wifidb/tools/utilites/reset_failed_finished_file.php
@@ -50,43 +50,197 @@ if($File_ID)
 		{
 			echo "Updating AP_ID".$AP_ID.")\r\n";
 			#Update AP IDs
-			if($dbcore->sql->service == "mysql")
-				{$sqlu = "UPDATE `wifi_ap` SET `File_ID` = ? WHERE `AP_ID` = ?";}
-			else if($dbcore->sql->service == "sqlsrv")
-				{$sqlu = "UPDATE [wifi_ap] SET [File_ID] = ? WHERE [AP_ID] = ?";}
-			echo "UPDATE [wifi_ap] SET [File_ID] = $New_File_ID WHERE [AP_ID] = $AP_ID\r\n";
-			$prep = $dbcore->sql->conn->prepare($sqlu);
-			$prep->bindParam(1, $New_File_ID, PDO::PARAM_INT);
-			$prep->bindParam(2, $AP_ID, PDO::PARAM_INT);
-			$prep->execute();
+			$retry = true;
+			while ($retry)
+			{
+				try 
+				{
+					if($dbcore->sql->service == "mysql")
+						{$sqlu = "UPDATE `wifi_ap` SET `File_ID` = ? WHERE `AP_ID` = ?";}
+					else if($dbcore->sql->service == "sqlsrv")
+						{$sqlu = "UPDATE [wifi_ap] SET [File_ID] = ? WHERE [AP_ID] = ?";}
+					echo "UPDATE [wifi_ap] SET [File_ID] = $New_File_ID WHERE [AP_ID] = $AP_ID\r\n";
+					$prep = $dbcore->sql->conn->prepare($sqlu);
+					$prep->bindParam(1, $New_File_ID, PDO::PARAM_INT);
+					$prep->bindParam(2, $AP_ID, PDO::PARAM_INT);
+					$prep->execute();
+					$retry = false;
+				}
+				catch (Exception $e) 
+				{
+					$retry = $dbcore->sql->isPDOException($dbcore->sql->conn, $e);
+				}
+			}
 		}
 	}
-	$sqlhp = "INSERT INTO files_tmp\n"
-		.	 "([file], file_orig, [user], otherusers, notes, title, size, date, hash, converted, prev_ext, type)\n"
-			. "SELECT [file], file_orig, [user], otherusers, notes, title, size, date, hash, converted, prev_ext, type\n"
-			. "FROM files\n"
-			. "WHERE id = ?";
-	$resgps = $dbcore->sql->conn->prepare($sqlhp);
-	$resgps->bindParam(1, $File_ID, PDO::PARAM_INT);
-	$resgps->execute();
-	
-	$sqlhp = "DELETE FROM wifi_hist WHERE File_ID = ?";
-	$resgps = $dbcore->sql->conn->prepare($sqlhp);
-	$resgps->bindParam(1, $File_ID, PDO::PARAM_INT);
-	$resgps->execute();	
-	
-	$sqlhp = "DELETE FROM wifi_ap WHERE File_ID = ?";
-	$resgps = $dbcore->sql->conn->prepare($sqlhp);
-	$resgps->bindParam(1, $File_ID, PDO::PARAM_INT);
-	$resgps->execute();
+		
+	#Go through Cells with this File ID
+	if($dbcore->sql->service == "mysql")
+		{$sql = "SELECT `cell_id` FROM `cell_id` WHERE file_id = ?";}
+	else if($dbcore->sql->service == "sqlsrv")
+		{$sql = "SELECT [cell_id]  FROM [cell_id] WHERE [file_id] = ?";}
+	$apl = $dbcore->sql->conn->prepare($sql);
+	$apl->bindParam(1, $File_ID, PDO::PARAM_INT);
+	$apl->execute();
+	while($ap = $apl->fetch(1))
+	{
+		$cell_id = $ap['cell_id'];
+		echo "cell_id:$cell_id\r\n";
+		
+		#Find if this AP is in another list
+		if($dbcore->sql->service == "mysql")
+			{$sqlhp = "SELECT `file_id` FROM `cell_hist` WHERE `cell_id` = ? And `file_id != ? LIMIT 1";}
+		else if($dbcore->sql->service == "sqlsrv")
+			{$sqlhp = "SELECT TOP 1 [file_id] FROM [cell_hist] WHERE [cell_id] = ? And [file_id] != ?";}
+		$resgps = $dbcore->sql->conn->prepare($sqlhp);
+		$resgps->bindParam(1, $cell_id, PDO::PARAM_INT);
+		$resgps->bindParam(2, $File_ID, PDO::PARAM_INT);
+		$resgps->execute();
+		$fetchgps = $resgps->fetch(2);
+		$New_File_ID = $fetchgps['file_id'];
+		if($New_File_ID)
+		{
+			echo "Updating cell_id".$cell_id.")\r\n";
+			#Update AP IDs
+			$retry = true;
+			while ($retry)
+			{
+				try 
+				{
+					if($dbcore->sql->service == "mysql")
+						{$sqlu = "UPDATE `cell_id` SET `file_id` = ? WHERE `cell_id` = ?";}
+					else if($dbcore->sql->service == "sqlsrv")
+						{$sqlu = "UPDATE [cell_id] SET [file_id] = ? WHERE [cell_id] = ?";}
+					$prep = $dbcore->sql->conn->prepare($sqlu);
+					$prep->bindParam(1, $New_File_ID, PDO::PARAM_INT);
+					$prep->bindParam(2, $cell_id, PDO::PARAM_INT);
+					$prep->execute();
+					$retry = false;
+				}
+				catch (Exception $e) 
+				{
+					$retry = $dbcore->sql->isPDOException($dbcore->sql->conn, $e);
+				}
+			}
 
-	$sqlhp = "DELETE FROM wifi_gps WHERE File_ID = ?";
-	$resgps = $dbcore->sql->conn->prepare($sqlhp);
-	$resgps->bindParam(1, $File_ID, PDO::PARAM_INT);
-	$resgps->execute();
+		}
+	}
+	$retry = true;
+	while ($retry)
+	{
+		try 
+		{
+			$sqlhp = "INSERT INTO files_tmp\n"
+				.	 "([file], file_orig, [user], otherusers, notes, title, size, date, hash, converted, prev_ext, type)\n"
+					. "SELECT [file], file_orig, [user], otherusers, notes, title, size, date, hash, converted, prev_ext, type\n"
+					. "FROM files\n"
+					. "WHERE id = ?";
+			$resgps = $dbcore->sql->conn->prepare($sqlhp);
+			$resgps->bindParam(1, $File_ID, PDO::PARAM_INT);
+			$resgps->execute();
+			$retry = false;
+		}
+		catch (Exception $e) 
+		{
+			$retry = $dbcore->sql->isPDOException($dbcore->sql->conn, $e);
+		}
+	}
 	
-	$sqlhp = "DELETE FROM files WHERE id = ?";
-	$resgps = $dbcore->sql->conn->prepare($sqlhp);
-	$resgps->bindParam(1, $File_ID, PDO::PARAM_INT);
-	$resgps->execute();
+	$retry = true;
+	while ($retry)
+	{
+		try 
+		{
+			$sqlhp = "DELETE FROM wifi_hist WHERE File_ID = ?";
+			$resgps = $dbcore->sql->conn->prepare($sqlhp);
+			$resgps->bindParam(1, $File_ID, PDO::PARAM_INT);
+			$resgps->execute();	
+			$retry = false;
+		}
+		catch (Exception $e) 
+		{
+			$retry = $dbcore->sql->isPDOException($dbcore->sql->conn, $e);
+		}
+	}
+	
+	$retry = true;
+	while ($retry)
+	{
+		try 
+		{
+			$sqlhp = "DELETE FROM wifi_ap WHERE File_ID = ?";
+			$resgps = $dbcore->sql->conn->prepare($sqlhp);
+			$resgps->bindParam(1, $File_ID, PDO::PARAM_INT);
+			$resgps->execute();
+			$retry = false;
+		}
+		catch (Exception $e) 
+		{
+			$retry = $dbcore->sql->isPDOException($dbcore->sql->conn, $e);
+		}
+	}
+	$retry = true;
+	while ($retry)
+	{
+		try 
+		{
+			$sqlhp = "DELETE FROM wifi_gps WHERE File_ID = ?";
+			$resgps = $dbcore->sql->conn->prepare($sqlhp);
+			$resgps->bindParam(1, $File_ID, PDO::PARAM_INT);
+			$resgps->execute();
+			$retry = false;
+		}
+		catch (Exception $e) 
+		{
+			$retry = $dbcore->sql->isPDOException($dbcore->sql->conn, $e);
+		}
+	}
+	$retry = true;
+	while ($retry)
+	{
+		try 
+		{
+			$sqlhp = "DELETE FROM cell_hist WHERE file_id = ?";
+			$resgps = $dbcore->sql->conn->prepare($sqlhp);
+			$resgps->bindParam(1, $File_ID, PDO::PARAM_INT);
+			$resgps->execute();
+			$retry = false;
+		}
+		catch (Exception $e) 
+		{
+			$retry = $dbcore->sql->isPDOException($dbcore->sql->conn, $e);
+		}
+	}
+	$retry = true;
+	while ($retry)
+	{
+		try 
+		{
+			$sqlhp = "DELETE FROM cell_id WHERE file_id = ?";
+			$resgps = $dbcore->sql->conn->prepare($sqlhp);
+			$resgps->bindParam(1, $File_ID, PDO::PARAM_INT);
+			$resgps->execute();
+			$retry = false;
+		}
+		catch (Exception $e) 
+		{
+			$retry = $dbcore->sql->isPDOException($dbcore->sql->conn, $e);
+		}
+	}
+	$retry = true;
+	while ($retry)
+	{
+		try 
+		{
+			$sqlhp = "DELETE FROM files WHERE id = ?";
+			$resgps = $dbcore->sql->conn->prepare($sqlhp);
+			$resgps->bindParam(1, $File_ID, PDO::PARAM_INT);
+			$resgps->execute();
+			$retry = false;
+		}
+		catch (Exception $e) 
+		{
+			$retry = $dbcore->sql->isPDOException($dbcore->sql->conn, $e);
+		}
+	}
 }
